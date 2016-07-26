bbd6d782e75af0390719eb6006b91bac444275aa
jmjriesen <jriesen17@masoncityschools.org>
2016-07-25 19:41:02 -0500

    redesigned how WeekGui created DayGuis

--- Schedule/src/CalenderGui/WeekGui.java
+++ Schedule/src/CalenderGui/WeekGui.java
@@ -8,29 +8,63 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class WeekGui {
-	int daysInWeek;
+	
+	//all days in this week week
+	DayGui[] daysGui = new DayGui[7];
+	
 	JPanel calendarPane = new JPanel();
 	GridLayout calendarLayout = new GridLayout(1,8);
 	JButton weekNumberButton;
 	
-	protected WeekGui(int weekNumber) {
-		constructer(weekNumber,7);	
-
-	}
-
-	public WeekGui(int weekNumber, int numberOfDaysInWeek) {
-		constructer(weekNumber, numberOfDaysInWeek);
-	}
-	
-	
-	private void constructer(int weekNumber, int numberOfDaysInWeek) {
+	protected WeekGui() {
 		// temp variable will evenchaly be replaced by a list of days
-		daysInWeek = numberOfDaysInWeek;
+		
 		//panal requierd for nesting gridlayouts
 		calendarPane.setLayout(calendarLayout);
 		// making a buton evenchaly will lead to week wide eddit window
-		weekNumberButton = new JButton("Week"+Integer.toString(weekNumber));
+		weekNumberButton = new JButton("Week");
 		calendarPane.add(weekNumberButton);
+
+	}
+	private void addDaysPanel() {
 		
+		
+		for(DayGui day:daysGui){
+			if(day==null){
+				calendarPane.add(new JLabel());
+			}else{
+				calendarPane.add(day.panel);
+				
+			}
+		}
+	}
+
+	public int makeDays(int monthStartsOn) {
+		int lastDayMade = 0;
+		for (int i = 0 ;i<(7-monthStartsOn);i++){
+			daysGui[7-(i+1)] = new DayGui(7-(monthStartsOn+i));
+			lastDayMade = i;
+		}
+		addDaysPanel();
+		
+		return lastDayMade+1;
+	}
+
+
+
+
+	public int makeDays(int DateWeekStartsOn, int daysInMonth) {
+		
+		int numberOfDaysMade = 0;
+		int lastDayMade = DateWeekStartsOn -1;
+		
+		while(numberOfDaysMade<7 && lastDayMade < daysInMonth){
+			
+			daysGui[numberOfDaysMade] = new DayGui(lastDayMade +1);
+			lastDayMade ++;
+			numberOfDaysMade ++;
+		}
+		addDaysPanel();
+		return lastDayMade;
 	}
 }
