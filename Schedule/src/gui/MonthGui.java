baac285f7033fe3b0d5ca396630b86d059b76394
jmjriesen <jriesen17@masoncityschools.org>
2016-08-02 18:03:42 -0500

    Added day of week labels and a button to edit the month

--- Schedule/src/gui/MonthGui.java
+++ Schedule/src/gui/MonthGui.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import dataManigment.Month;
@@ -13,16 +16,16 @@ import dataManigment.Week;
 public class MonthGui {
 	private Month month;
 	private WeekGui[] weekGuis;
-	
-	
+
+
 	private JPanel monthDesplay;
 
 
 	public MonthGui(Month month) {
-		
+
 		//month that is to be despladed
 		this.month = month;
-		
+
 		//creating Weekguis for each week in month
 		weekGuis = new WeekGui[month.getWeeks().length];
 		for (int index = 0; index<month.getWeeks().length; index++){
@@ -30,16 +33,54 @@ public class MonthGui {
 				weekGuis[index] = new WeekGui(month.getWeeks()[index]); 
 			}
 		}
-		
-		
-		
+
+
+
 		monthDesplay = new JPanel();
-		monthDesplay.setLayout(new GridLayout(weekGuis.length,1));
+		//the plus two is for the top and bottom labels
+		monthDesplay.setLayout(new GridLayout(weekGuis.length + 2 ,1));
+		addFirstRow();
+
+
+
+
+
+
+
 
 		for (WeekGui weekgui : weekGuis){
 			monthDesplay.add(weekgui.getThumbnailPanel());
-			
+
 		}
+
+	}
+
+	private void addFirstRow() {
+		JPanel firstRow = new JPanel();
+		firstRow.setLayout(new GridLayout(1,8));
+		JButton editMonthButton = new JButton("Edit");
+		editMonthButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				GuiControl.getInstence().startCalenderSettingsWindow();				
+			}
+
+		});
+		firstRow.add(editMonthButton);
+		firstRow.add(new JLabel("Sunday"));
+		firstRow.add(new JLabel("Monday"));
+		firstRow.add(new JLabel("Tusday"));
+		firstRow.add(new JLabel("Wendnesday"));
+		firstRow.add(new JLabel("Thrusday"));
+		firstRow.add(new JLabel("Friday"));
+		firstRow.add(new JLabel("Saterday"));
+
+		this.monthDesplay.add(firstRow);
+
+
+
+
 
 	}
 
